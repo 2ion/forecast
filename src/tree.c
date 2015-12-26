5ac6eedb05fefaa7216055f4c00419909c2070e3
Jens John <dev@2ion.de>
2015-12-26 20:37:51 +0900

    tree: implemen tree_free()

--- src/tree.c
+++ src/tree.c
@@ -9,6 +9,104 @@ static TData**      parse_daily_object(struct json_object*, TALLOC_CTX*, size_t*
 
 /*********************************************************************/
 
+TLocation* tree_new(const Data *d)
+{
+  TLocation *l;
+  struct json_object *o, *oo, *ooo;
+  struct array_list *al;
+
+  if((o = json_tokener_parse(d->data)) == NULL)
+    return NULL;
+
+  if((l = talloc(NULL, TLocation)) == NULL)
+    return NULL;
+
+  /* top-level data */
+
+  l->timezone = access_string(o, "timezone", l);
+  l->latitude = access_double(o, "latitude");
+  l->longitude = access_double(o, "longitude");
+  l->offset = access_int(o, "offset");
+
+  /* .currently */
+
+  json_object_object_get_ex(o, "currently", &oo);
+  l->w_currently = parse_hourly_object(o, l, &l->w_currently_len);
+
+  /* .hourly */
+
+  json_object_object_get_ex(o, "hourly", &oo);
+  json_object_object_get_ex(oo, "summary", &ooo);
+  l->w_hourly_summary = talloc_strdup(l, json_object_get_string(ooo));
+  json_object_object_get_ex(oo, "data", &ooo);
+  al = json_object_get_array(ooo);
+  l->w_hourly_len = array_list_length(al);
+  l->w_hourly_chld_len = talloc_array(l, size_t, l->w_hourly_len);
+  l->w_hourly = talloc_array(l, TData**, l->w_hourly_len);
+  for(size_t i = 0; i < l->w_hourly_len; i++)
+  {
+    struct json_object *p = array_list_get_idx(al, i);
+    l->w_hourly[i] = parse_hourly_object(p, l->w_hourly, &(l->w_hourly_chld_len[i]));
+  }
+
+  /* .daily */
+
+  json_object_object_get_ex(o, "daily", &oo);
+  json_object_object_get_ex(oo, "summary", &ooo);
+  l->w_daily_summary = talloc_strdup(l, json_object_get_string(ooo));
+  json_object_object_get_ex(oo, "data", &ooo);
+  al = json_object_get_array(ooo);
+  l->w_daily_len = array_list_length(al);
+  l->w_daily_chld_len = talloc_array(l, size_t, l->w_daily_len);
+  l->w_daily = talloc_array(l, TData**, l->w_daily_len);
+  for(size_t i = 0; i < l->w_daily_len; i++)
+  {
+    struct json_object *p = array_list_get_idx(al, i);
+    l->w_daily[i] = parse_daily_object(p, l->w_daily, &(l->w_daily_chld_len[i]));
+  }
+
+  /* .flags */
+
+  json_object_object_get_ex(o, "flags", &oo);
+  l->units = access_string(oo, "units", l);
+
+  /* free json-c data structures by decreasing the refcount */
+  json_object_put(o);
+
+  return l;
+}
+
+void tree_free(TLocation *l)
+{
+  talloc_free(l);
+}
+
+int* tree_int(TData **t, size_t tlen, const char *key)
+{
+  for(size_t i = 0; i < tlen; i++)
+    if(strcmp(t[i]->name, key) == 0)
+      return &(t[i]->value.i);
+  return NULL;
+}
+
+char* tree_char(TData **t, size_t tlen, const char *key)
+{
+  for(size_t i = 0; i < tlen; i++)
+    if(strcmp(t[i]->name, key) == 0)
+      return t[i]->value.s;
+  return NULL;
+}
+
+double* tree_float(TData **t, size_t tlen, const char *key)
+{
+  for(size_t i = 0; i < tlen; i++)
+    if(strcmp(t[i]->name, key) == 0)
+      return &(t[i]->value.d);
+  return NULL;
+}
+
+/*********************************************************************/
+
 TData** parse_daily_object(struct json_object *o, TALLOC_CTX *parent, size_t *alen)
 {
   TData **array;
@@ -91,98 +189,5 @@ double access_double(struct json_object *o, const char *key)
   if(json_object_object_get_ex(o, key, &oo) != TRUE)
     return 0;
   return json_object_get_double(oo);
-}
-
-/*********************************************************************/
-
-int* tree_int(TData **t, size_t tlen, const char *key)
-{
-  for(size_t i = 0; i < tlen; i++)
-    if(strcmp(t[i]->name, key) == 0)
-      return &(t[i]->value.i);
-  return NULL;
-}
-
-char* tree_char(TData **t, size_t tlen, const char *key)
-{
-  for(size_t i = 0; i < tlen; i++)
-    if(strcmp(t[i]->name, key) == 0)
-      return t[i]->value.s;
-  return NULL;
-}
-
-double* tree_float(TData **t, size_t tlen, const char *key)
-{
-  for(size_t i = 0; i < tlen; i++)
-    if(strcmp(t[i]->name, key) == 0)
-      return &(t[i]->value.d);
-  return NULL;
-}
-
-TLocation* tree_new(const Data *d)
-{
-  TLocation *l;
-  struct json_object *o, *oo, *ooo;
-  struct array_list *al;
-
-  if((o = json_tokener_parse(d->data)) == NULL)
-    return NULL;
-
-  if((l = talloc(NULL, TLocation)) == NULL)
-    return NULL;
-
-  /* top-level data */
-
-  l->timezone = access_string(o, "timezone", l);
-  l->latitude = access_double(o, "latitude");
-  l->longitude = access_double(o, "longitude");
-  l->offset = access_int(o, "offset");
-
-  /* .currently */
-
-  json_object_object_get_ex(o, "currently", &oo);
-  l->w_currently = parse_hourly_object(o, l, &l->w_currently_len);
-
-  /* .hourly */
-
-  json_object_object_get_ex(o, "hourly", &oo);
-  json_object_object_get_ex(oo, "summary", &ooo);
-  l->w_hourly_summary = talloc_strdup(l, json_object_get_string(ooo));
-  json_object_object_get_ex(oo, "data", &ooo);
-  al = json_object_get_array(ooo);
-  l->w_hourly_len = array_list_length(al);
-  l->w_hourly_chld_len = talloc_array(l, size_t, l->w_hourly_len);
-  l->w_hourly = talloc_array(l, TData**, l->w_hourly_len);
-  for(size_t i = 0; i < l->w_hourly_len; i++)
-  {
-    struct json_object *p = array_list_get_idx(al, i);
-    l->w_hourly[i] = parse_hourly_object(p, l->w_hourly, &(l->w_hourly_chld_len[i]));
-  }
-
-  /* .daily */
-
-  json_object_object_get_ex(o, "daily", &oo);
-  json_object_object_get_ex(oo, "summary", &ooo);
-  l->w_daily_summary = talloc_strdup(l, json_object_get_string(ooo));
-  json_object_object_get_ex(oo, "data", &ooo);
-  al = json_object_get_array(ooo);
-  l->w_daily_len = array_list_length(al);
-  l->w_daily_chld_len = talloc_array(l, size_t, l->w_daily_len);
-  l->w_daily = talloc_array(l, TData**, l->w_daily_len);
-  for(size_t i = 0; i < l->w_daily_len; i++)
-  {
-    struct json_object *p = array_list_get_idx(al, i);
-    l->w_daily[i] = parse_daily_object(p, l->w_daily, &(l->w_daily_chld_len[i]));
-  }
-
-  /* .flags */
-
-  json_object_object_get_ex(o, "flags", &oo);
-  l->units = access_string(oo, "units", l);
-
-  /* free json-c data structures by decreasing the refcount */
-  json_object_put(o);
-
-  return l;
 }
 
