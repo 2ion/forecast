0f0b86f68184369ed2c9bfa51c3e4e026899e026
Jens John <dev@2ion.de>
2016-01-03 00:26:51 +0900

    lerror: Move global LERROR() definition to lerror.h

--- src/lerror.h
+++ src/lerror.h
@@ -1,23 +1,25 @@
 #ifndef LERROR_H
 #define LERROR_H
 
-/*
- * a portable re-implementation of GNU libc's error_at_line(), providing
- * LERROR(). This should build against OSX's libc -- to be tested.
- */
-
-#include <errno.h>
-#include <string.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include <talloc.h>
+
+#include "config.h"
 
 void lerror(const char *file_name, const char *func_name, unsigned int file_line, int status,
     int errnum, FILE *stream, const char *msg, ...);
 
+#ifdef HAVE_ERROR_AT_LINE
+#include <errno.h>
+#include <error.h>
+#define LERROR(status, errnum, ...) error_at_line((status), (errnum), \
+        (__func__), (__LINE__), __VA_ARGS__)
+#else
 #define LERROR(status, errnum, ...) lerror((__FILE__), (__func__), (__LINE__), \
     (status), (errnum), stderr, __VA_ARGS__)
+#endif
 
 #endif
